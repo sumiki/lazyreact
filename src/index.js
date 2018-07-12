1f6c4d37f39d0fdbc36c6b87cce9c3b98a4a32e0
Sumiki Ohsawa <sumikio@gmail.com>
2018-07-11 22:54:23 -0700

    output management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = "TEST";
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
