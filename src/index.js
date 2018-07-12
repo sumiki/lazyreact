d79aa0bbcf713d54bad4e6e032ab75eb2944690c
Sumiki Ohsawa <sumikio@gmail.com>
2018-07-11 23:24:23 -0700

    async

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-        var element = document.createElement('div');
-        var _ = _.default;
-        element.innerHTML = _.join(['Hello!', 'webpack'], ' ');
-        return element;
-    }).catch(error => 'An error occurred while loading the component');
+async function getComponent() {
+    var element = document.createElement('div');
+    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 }
 
 getComponent().then(component => {
